ef5a030b6914c1b65ab3e734eaca40b8e449c314
kawshar798 <kawshar798@gmail.com>
2020-04-21 23:49:06 +0600

    update

--- src/components/Minute.js
+++ src/components/Minute.js
@@ -3,8 +3,35 @@ import Button from '@material-ui/core/Button';
 import Badge from '@material-ui/core/Badge';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Input from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import './Minute.css'
+
+
+
 const Minute = () => {
     const [minutes, setMinutes] = useState(5);
+    const [error, setError] = useState(null);
+    const [selectItem,setSelectItem] = useState("login");
+
+    const handleMinus = () => {
+        if (minutes > 1) {
+            setMinutes(minutes - 1)
+        } else {
+            setError("Greater then 0 plz 😡")
+        }
+
+    }
+
+    const handleAdd = () => {
+        setMinutes(minutes + 1)
+        setError(null);
+    }
     return (
         <div>
 
@@ -15,19 +42,53 @@ const Minute = () => {
                 <Button onClick={() => setMinutes(minutes + 1)}>+</Button>
             </div> */}
 
-            <div style={{ display:'flex',width:'100%',justifyContent:'center',padding: '100px',boxSizing:'border-box'}}>
-            <Button onClick={() => { setMinutes(minutes <= 1 ? 1 : minutes - 1) }}><RemoveIcon/></Button>
-            <Badge badgeContent={minutes} color="secondary">Minutes</Badge>
-            <Button onClick={() => { setMinutes(minutes + 1) }}><AddIcon/></Button>
+            <div style={{ display: 'flex', width: '100%', justifyContent: 'center', padding: '100px', boxSizing: 'border-box' }}>
+                <Button onClick={handleMinus}><RemoveIcon /></Button>
+                <Badge badgeContent={minutes} color="secondary">Minutes</Badge>
+                <Button onClick={handleAdd}><AddIcon /></Button>
+
             </div>
+            <div><p>{error}</p></div>
+            <Container>
+                <Grid item xs={3}>
+                    <Card>
+                        <CardContent>
+                            <Button onClick={()=>setSelectItem("login")} className={selectItem==="login"?'active':''}>
+                                Login
+                                </Button>
+                                
+                                <Button onClick={()=>setSelectItem("logout")} className={selectItem==="logout"?'active':''}>Logout</Button>
+                        </CardContent>
+                        <div   className={selectItem==="login"?' login tab-active':'login'}>
+                        <input type="text"placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <Checkbox
+                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                        />
+                         <Button>Login</Button>
+                        </div>
+
+
+                        <div   className={selectItem==="logout"?' signup tab-active':'signup'}>
+                        <input type="text" placeholder="Name " />
+                        <input type="email" placeholder="Email"/>
+                        <input type="password" placeholder="Password" />
+                        <Checkbox
+                            inputProps={{ 'aria-label': 'primary checkbox' }}
+                        />
+                         <Button>Sign Up</Button>
+                        </div>
+                        
+                        
+                    </Card>
+                </Grid>
+
+            </Container>
 
 
         </div>
     );
 };
 
-img.tp-kbimg {
-    height: 300px!important;
-}
 
 export default Minute;
